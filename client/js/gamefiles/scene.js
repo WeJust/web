c587009a229788cd3b53188cbef74249cb99732a
tomi <lair.thomas2010@gmail.com>
2017-01-11 18:02:38 +0100

    test

--- client/js/gamefiles/scene.js
+++ client/js/gamefiles/scene.js
@@ -4,18 +4,18 @@ var createScene = function() {
     var scene = new BABYLON.Scene(engine);
     scene.collisionsEnabled = true;
     scene.enablePhysics();
-    
+
     /****************** Preload **********************/
     var mat = materials(scene); // load all materials
     var go = gameobjects(scene,mat); // load all game objects
     var cam = camera(scene,go); // load camera(s)
     var lig = light(scene); // load light(s)
     /******************END Preload **********************/
-    
-    
+
+
     inputevents(scene,go);
-    
+
     update(scene,engine,mat,go,cam,lig);
-    
+
     return scene;
 }
