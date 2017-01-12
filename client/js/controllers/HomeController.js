58de4a98ee0f1f25ad3804c8f9961279dd2c5718
tomi <lair.thomas2010@gmail.com>
2017-01-12 17:56:44 +0100

    aa

--- client/js/controllers/HomeController.js
+++ client/js/controllers/HomeController.js
@@ -1,3 +1,3 @@
-angular.module('app', ['ngRoute']).controller('HomeController', ['$scope', function ($scope) {
+angular.module('app').controller('HomeController', ['$scope', function ($scope) {
       $scope.test = "test var";
     }]);
