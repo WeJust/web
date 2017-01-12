d5a8d4e2a1a2aaa98deb33b73461705f2c79c869
tomi <lair.thomas2010@gmail.com>
2017-01-12 17:01:51 +0100

    aa

--- client/js/controllers/HomeController.js
+++ client/js/controllers/HomeController.js
@@ -1,3 +1,3 @@
-app.controller('HomeController', ['$scope', function ($scope) {
+angular.module('app', ['ngRoute']).controller('HomeController', ['$scope', function ($scope) {
       $scope.test = "test var";
     }]);
--- client/js/routes.js
+++ client/js/routes.js
@@ -1,6 +1,4 @@
-var app = angular.module('app', ['ngRoute']);
-
-app.config(['$routeProvider', function ($routeProvider) {
+angular.module('app', ['ngRoute']).config(['$routeProvider', function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: '/pages/login.html',
