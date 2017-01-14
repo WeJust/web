05345b6ed6b00c9eeaa6bbd7e1b51d412e60f533
adrien <adrien.sanchez@eisti.eu>
2017-01-14 11:08:09 +0100

    Test register 3

--- client/js/routes.js
+++ client/js/routes.js
@@ -1,11 +1,15 @@
 angular.module('app', ['ngRoute']).config(['$routeProvider', function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: '/pages/login.html'
-      })
-      .when('/test', {
-          templateUrl: '/pages/signup.html',
-          controller: 'HomeController'
+        .when('/', {
+            templateUrl: '/pages/login.html'
+        })
+        .when('/register',{
+            templateUrl: '/pages/signup.html'
+        })
+        .when('/test', {
+            templateUrl: '/pages/signup.html',
+            controller: 'HomeController'
 
         });
+
 }]);
