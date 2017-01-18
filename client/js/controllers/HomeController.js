453ae245d79e6b1f492e8a1f9b4da527bac35edd
adrien <adrien.sanchez@eisti.eu>
2017-01-18 16:39:57 +0100

    check username

--- client/js/controllers/HomeController.js
+++ client/js/controllers/HomeController.js
@@ -1,4 +1,32 @@
-angular.module('app').controller('HomeController', ['$scope', function ($scope) {
+angular.module('app',["firebase"]).controller('HomeController', function ($scope,$firebaseObject) {
       $scope.test = "test var";
+      $scope.usernameValid = true;
+      $scope.checkUsername=function () {
+          var ref = firebase.database().ref("username/" +  $scope.usernameField);
+          // download the data into a local object
+          $scope.data = $firebaseObject(ref);
+          ref.once('value').then(function (snapshot) {
+              if (snapshot.val() != null) {
+                $scope.usernameValid = false;
+              } else {
+                  $scope.usernameValid = true;
+              }
+          })
+      };
+      
+      $scope.authentication = function () {
+          var email = $scope.emailField;
+          var password = $scope.passwordField;
+          var username = $scope.usernameField;
 
-    }]);
+          firebase.auth().createUserWithEmailAndPassword(email,password).catch(function (error) {
+              var errorCode = error.code;
+              var errorMessage = error.message;
+              return "Error ! " + errorCode + " : "+ errorMessage;
+          });
+
+          var ref = firebase.database().ref("username").set({
+              username: username
+          });
+      }
+    });
